e8007bcfcc17251a10057e7ff72af3890458c24d
kennedy0919 <kennedy1234@naver.com>
2023-07-16 20:17:54 +0900

    새로운 커밋ㅇ

--- 가위바위보게임.py
+++ 가위바위보게임.py
@@ -1,3 +1,4 @@
+'''
 import random
 
 gamecode = ["가위", "바위", "보"]
@@ -28,4 +29,19 @@ elif user == "보" and com == "바위":
 
 elif user == "보" and com == "가위":
     print("유저가", user,"를 내고","컴퓨터가", com,"을 내서 졌습니다.")
+'''
+A,B = map(int, input().split())
+if (A !=2) and (B !=2):
+    if A == 1:
+        print("A")
+    else :
+        print("B")    
 
+else :
+    if A > B :
+        print("A")
+    else :
+        print("B")
+
+# map(함수, 리스트)
+# lambda(매개변수, 함수)
