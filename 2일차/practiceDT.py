06ab3d63782a724aec78ce4e04acf6784cab42f6
kennedy <kennedy1234@naver.com>
2023-07-18 11:58:32 +0900

    2일차 커밋

---
+++ 2일차/practiceDT.py
@@ -0,0 +1,44 @@
+# 진법 변경
+print(bin(12))
+
+
+# 지수 표현 방식(제곱하는 횟수)
+# print(314e-2)
+# print(314e2)
+
+# print("Debugging roachse 13 living room")
+
+# bugs = "roachse"
+# counts = 13
+# area = "living room"
+# print(f"Debugging {bugs} {counts} {area}")
+# print("Debugging %s %d %s" % (bugs, counts, area))
+
+# # f-string 응용
+# greeting = "hi"
+# print(f"{greeting:^10}")
+# print(f"{greeting:>10}")
+# print(f"{3.141592:.4f}")
+
+# my_str = " hello"
+# my_str[0] = "z"
+
+# my_list[1, 2, 3]
+# my_list[0] = 1000
+
+# 푸후후
+
+# 단축평가 시험에 나온다 이거
+
+# 단축평가의 이유 : 코드 실행을 최적화하고 불필요한 연산을 피할 수 있도록 함
+
+# world = "hello"
+# numbers = [1, 2, 3, 4, 5]
+
+# print("h" in world)
+
+dic = {"name": "kennedy", "height": 175}
+
+print(list(dic))
+
+# key 값만 들어오고 value 값은 버려진다.
