3da2457fc83d05704b50b3af42f2e1d5bc8f7985
kennedy0919 <kenendy1234@naver.com>
2023-08-18 17:51:54 +0900

    24일차 커밋

--- 22일차/View.py
+++ 22일차/View.py
@@ -5,7 +5,7 @@ for tc in range(1, T + 1):
     building = list(map(int, input().split()))
     ans = 0
     
-    for i in range(0, N - 1):
+    for i in range(2, N):
         if building[i] - building[i-1] > 0 and building[i] - building[i-2] > 0 and building[i] - building[i+1] > 0 and building[i] - building[i+2] > 0:
             list_building = [building[i-1], building[i-2], building[i+1], building[i+2]]
             ans = ans + building[i] - max(list_building)
